35ac65d50fd46e2bc658a519311fca714f6a6bfb
Bukky <makinwa37@gmail.com>
2020-02-13 15:44:11 +0100

    Set up client app

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+const apiId = 'bdqk4bjb9k'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-jn1ijedp.eu.auth0.com',            // Auth0 domain
+  clientId: '9WgLWhUsntFM5hqyGlH0qRtS3ltnB1C4',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
