fb137b73647143e59138fb2974459aabed5dd08b
Makinwa Olubukola <makinwa37@gmail.com>
2020-02-14 07:54:23 +0100

    Implement creation via webapp

--- client/src/components/Trips.tsx
+++ client/src/components/Trips.tsx
@@ -3,14 +3,14 @@ import { History } from 'history'
 import * as React from 'react'
 import {
   Button,
-  Checkbox,
   Divider,
   Grid,
   Header,
   Icon,
   Input,
-  Image,
-  Loader
+  Loader,
+  Form,
+  FormGroup
 } from 'semantic-ui-react'
 
 import { createTrip, getTrips } from '../api/trips-api'
@@ -55,9 +55,8 @@ export class Trips extends React.PureComponent<TripsProps, TripsState> {
     this.props.history.push(`/trips/${tripId}/edit`)
   }
 
-  onTripCreate = async (event: React.ChangeEvent<HTMLButtonElement>) => {
+  onTripCreate = async (event: React.MouseEvent<HTMLButtonElement>) => {
     try {
-      // const date = this.calculateDueDate()
       const newTrip = await createTrip(this.props.auth.getIdToken(), {
         location: this.state.newTripLocation,
         date: this.state.newTripDate,
@@ -65,7 +64,6 @@ export class Trips extends React.PureComponent<TripsProps, TripsState> {
       })
       this.setState({
         trips: [...this.state.trips, newTrip],
-        newTripLocation: ''
       })
     } catch {
       alert('Trip creation failed')
@@ -99,49 +97,32 @@ export class Trips extends React.PureComponent<TripsProps, TripsState> {
   renderCreateTripInput() {
     return (
       <Grid.Row>
-        <Grid.Column width={16}>
-          <Input
-            action={{
-              color: 'teal',
-              labelPosition: 'left',
-              icon: 'add',
-              content: 'New trip location',
-              onClick: this.onTripCreate
-            }}
-            fluid
-            actionPosition="left"
-            placeholder="To change the world..."
-            onChange={this.handleTripLocationChange}
-          />
-
-          <Input
-            action={{
-              color: 'teal',
-              labelPosition: 'left',
-              icon: 'add',
-              content: 'Budget for trip (EUR)',
-              onClick: this.onTripCreate
-            }}
-            fluid
-            actionPosition="left"
-            placeholder="1000"
-            onChange={this.handleBudgetChange}
-          />
-
-          <Input
-            action={{
-              color: 'teal',
-              labelPosition: 'left',
-              icon: 'add',
-              content: 'Date of trip',
-              onClick: this.onTripCreate
-            }}
-            fluid
-            type="datetime"
-            actionPosition="left"
-            onChange={this.handleDateChange}
-          />
-        </Grid.Column>
+        <Form size='large'>
+          <Form.Group>
+            <Form.Field>
+              <label>Location</label>
+              <Input
+                placeholder="To change the world..."
+                onChange={this.handleTripLocationChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Budget</label>
+              <Input
+                placeholder="1000"
+                onChange={this.handleBudgetChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Date of trip</label>
+              <Input
+                type="date"
+                onChange={this.handleDateChange}
+              />
+            </Form.Field>
+            <Button primary type='submit' onClick={this.onTripCreate}>Create new Trip</Button>
+          </Form.Group>
+        </Form>
         <Grid.Column width={16}>
           <Divider />
         </Grid.Column>
@@ -170,41 +151,57 @@ export class Trips extends React.PureComponent<TripsProps, TripsState> {
   renderTripsList() {
     return (
       <Grid padded>
+        <Grid.Row>
+          <Grid.Column width={5} verticalAlign="middle" floated="left">
+            <b>Location</b>
+          </Grid.Column>
+
+          <Grid.Column width={5} verticalAlign="middle" floated="left">
+            <b>Budget</b>
+          </Grid.Column>
+
+          <Grid.Column width={3} verticalAlign="middle" floated="left">
+            <b>Date</b>
+          </Grid.Column>
+        </Grid.Row>
         {(this.state.trips && this.state.trips.length > 0) ? this.state.trips.map((trip, pos) => {
           return (
-            <Grid.Row key={trip.tripId}>
-              <Grid.Column width={1} verticalAlign="middle">
-              </Grid.Column>
-              <Grid.Column width={10} verticalAlign="middle">
-                {trip.location}
-              </Grid.Column>
-              <Grid.Column width={10} verticalAlign="middle">
-                {trip.budget} EUR
-              </Grid.Column>
-              <Grid.Column width={3} floated="right">
-                {dateFormat(trip.date, 'yyyy-mm-dd')}
-              </Grid.Column>
-              <Grid.Column width={1} floated="right">
-                <Button
-                  icon
-                  color="blue"
-                  onClick={() => this.onEditButtonClick(trip.tripId)}
-                >
-                  <Icon name="pencil" />
-                </Button>
-              </Grid.Column>
-              <Grid.Column width={16}>
-                <Divider />
-              </Grid.Column>
-            </Grid.Row>
+            <React.Fragment>
+              <Grid.Row key={trip.tripId}>
+                <Grid.Column width={1} verticalAlign="middle">
+                </Grid.Column>
+                <Grid.Column width={5} verticalAlign="middle" floated="left">
+                  {trip.location}
+                </Grid.Column>
+                <Grid.Column width={5} verticalAlign="middle" floated="left">
+                  {trip.budget} EUR
+                </Grid.Column>
+                <Grid.Column width={3} verticalAlign="middle" floated="left">
+                  {trip.date}
+                </Grid.Column>
+                <Grid.Column width={1} floated="right">
+                  <Button
+                    icon
+                    color="blue"
+                    onClick={() => this.onEditButtonClick(trip.tripId)}
+                  >
+                    <Icon name="pencil" />
+                  </Button>
+                </Grid.Column>
+                <Grid.Column width={16}>
+                  <Divider />
+                </Grid.Column>
+              </Grid.Row>
+            </React.Fragment>
           )
         }) : <h3>There are currently no Trips.</h3>}
       </Grid>
     )
   }
 
-  calculateDueDate(): string {
-    const date = new Date()
+  parseDate(tripDate: string): string {
+    console.log(tripDate, 'sup??')
+    const date = new Date(tripDate)
     date.setDate(date.getDate() + 7)
 
     return dateFormat(date, 'yyyy-mm-dd') as string
